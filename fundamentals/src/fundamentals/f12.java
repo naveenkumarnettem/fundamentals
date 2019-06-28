6600df1481fd7e1f4a8b57828f19fd501859293e
vempa <vempa@Lappy>
2019-06-28 11:48:08 +0530

    first

---
+++ fundamentals/src/fundamentals/f12.java
@@ -0,0 +1,15 @@
+package fundamentals;
+import java.util.*;
+public class f12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++)
+		{
+			System.out.print(i+"\t");
+		}
+	
+
+	}
+
+}
